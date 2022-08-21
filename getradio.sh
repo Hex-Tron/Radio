96fcfd6c4eaf65e7b910ffe665ac9d00ea3740f1
Hex-Tron <xredbro@gmail.com>
2022-08-21 11:30:13 +0530

    update

--- getradio.sh
+++ getradio.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source nai.sh
+source ./nai.sh
 curl "http://$url/json/servers" \
   -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
   -H 'Accept-Language: en-US,en;q=0.9' \
